92a9f116ae1d181f7cd9b825dc4bcc341699d3c9
Anshul Singh <anshulsingh024@yahoo.com>
2024-09-23 00:04:26 +0530

    Completed Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,24 @@
-import React from 'react'
+import React from "react";
 
 const Navbar = () => {
   return (
-    <div>
-      navbar
-    </div>
-  )
-}
+    <nav className=" px-24 py-8 bg-slate-100">
+      <div className="flex justify-between align-middle">
+        <div className="icon text-2xl">
+        <a href="/">
+          <span className="">anshul</span>
+          <span className=" text-blue-600">.</span>
+          </a>
+        </div>
+        <ul className="flex text-lg space-x-6">
+          <li><a href="#home">Home</a></li>
+          <li><a href="#about">About</a></li>
+          <li><a href="#skills">Skills</a></li>
+          <li><a href="#projects">Projects</a></li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
