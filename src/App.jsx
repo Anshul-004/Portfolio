45d65a39ca0e23e123ec811f26d8f9f6908d78c3
Anshul Singh <anshulsingh024@yahoo.com>
2024-09-22 23:29:04 +0530

    Added Font POPPINS

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,12 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar.jsx'
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <div className='text-3xl font-bold text-center'>
-        Hi, I'm Anshul
-      </div>
-      <p>This is my portfolio</p>
+      <Navbar />
     </>
   )
 }
