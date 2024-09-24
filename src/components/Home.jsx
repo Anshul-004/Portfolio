fa5674c06c980f1d45e9c0c807068c07e9372e6f
Anshul Singh <anshulsingh024@yahoo.com>
2024-09-24 19:44:53 +0530

    Updated Design

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,38 +1,17 @@
-import React, { useEffect, useRef } from "react";
+import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faSortDown } from "@fortawesome/free-solid-svg-icons";
-import rough from "roughjs";
 
 const Home = () => {
-  const canvasRef = useRef(null);
-
-  useEffect(() => {
-    const canvas = canvasRef.current;
-    const context = canvas.getContext("2d");
-    const rc = rough.canvas(canvas);
-
-    rc.rectangle(90, 20, 220, 80, {
-      roughness: 2,
-      stroke: "purple",
-      strokeWidth: 2,
-    });
-  }, []);
-
   return (
     <div
       className="relative flex flex-col justify-center px-24 py-8 h-screen"
       id="home"
     >
-      <canvas
-        ref={canvasRef}
-        width="400"
-        height="110"
-        className="absolute top-1/4 left-1/4 z-0"
-      ></canvas>
-
-      <div className="TextBox relative z-8 text-6xl w-2/3 mb-6 leading-snug">
+      <div className="TextBox z-8 text-6xl w-2/3 mb-6 leading-snug">
         <span className="text-purple-500"> Hi There.</span> I'm{" "}
-        <span className="relative">Anshul,</span> a CS Undergrad and a Full-Stack Developer.
+        <span className="underline decoration-purple-400 decoration-wavy">Anshul,</span> a CS Undergrad and a Full-Stack
+        Developer.
       </div>
 
       <div className="text-xl pt-2">I write code for a living.</div>
